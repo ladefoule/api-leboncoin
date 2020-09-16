03a9fe10d5a23cb5dfa1a94a0a9288058fbafab0
Moussa <moussa@976>
2020-09-16 11:09:33 +0200

    TACHE 5 OK : Type de requete envoyé et envoi de réponse HTTP

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
 
+// header('Content-Type: application/html', TRUE, 200);
 
-var_dump($_SREVER);
+header('Content-Type: application/json');
+header('HTTP/1.0 200 Not Found');
+
+echo json_encode($_SERVER);
+echo json_encode($_SERVER['REQUEST_METHOD']);
