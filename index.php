0383bce46a50d57b880b700efef64813747df66c
Moussa <moussa@976>
2020-09-16 11:38:43 +0200

    TACHE 2 OK : STRUCTURE DE LA BASE

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php 
 
-// header('Content-Type: application/html', TRUE, 200);
-
+// On définit le header de retour qui sera du JSON
 header('Content-Type: application/json');
-header('HTTP/1.0 200 Not Found');
 
-echo json_encode($_SERVER);
-echo json_encode($_SERVER['REQUEST_METHOD']);
+// Les différents retours HTTP possibles
+header('HTTP/1.0 200 OK');
+header('HTTP/1.0 400 Bad Request');
+header('HTTP/1.0 401 Unauthorized');
+header('HTTP/1.0 404 Not Found');
+header('HTTP/1.0 405 Method Not Allowed');
+
+// Pour récupérer le méthode de la requête
+$_SERVER['REQUEST_METHOD'];
